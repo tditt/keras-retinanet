e1841d72c49d5e463d3fe2d99ddad7fa21a485d0
Timo Dittus <timo.dittus@denkbares.com>
2018-08-26 13:58:56 +0200

    add some bigger anchor scales to improve detection of large objects

--- keras_retinanet/utils/anchors.py
+++ keras_retinanet/utils/anchors.py
@@ -258,7 +258,7 @@ def generate_anchors(base_size=16, ratios=None, scales=None):
     if ratios is None:
         ratios = np.array([0.7, 1, 1.3])
     if scales is None:
-        scales = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+        scales = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.2, 1.5])
 
     num_anchors = len(ratios) * len(scales)
 
