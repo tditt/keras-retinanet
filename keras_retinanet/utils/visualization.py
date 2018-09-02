7970972b30665bd75312dc26c73e59576de8a6bb
Timo Dittus <timo.dittus@denkbares.com>
2018-09-02 15:26:23 +0200

    prevent object class from being drawn into evaluation pictures

--- keras_retinanet/utils/visualization.py
+++ keras_retinanet/utils/visualization.py
@@ -78,7 +78,8 @@ def draw_detections(image, boxes, scores, labels, color=None, label_to_name=None
         draw_box(image, boxes[i, :], color=c)
 
         # draw labels
-        caption = (label_to_name(labels[i]) if label_to_name else labels[i]) + ': {0:.2f}'.format(scores[i])
+        # caption = (label_to_name(labels[i]) if label_to_name else labels[i]) + ': {0:.2f}'.format(scores[i])
+        caption = ': {0:.2f}'.format(scores[i])
         draw_caption(image, boxes[i, :], caption, color=(0, 255, 0))
 
 
